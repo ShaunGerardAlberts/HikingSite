9041646b4e1a21d16a8ff55e51323a741cc80e63
DESKTOP-N7QL03N\Shaun <gerard.alberts@hotmail.com>
2017-06-08 17:18:25 +1000

    Site is starting to take shape.  Added scroll jquery plauging.  All functionality is working as required.  Appearance needs work.

--- HikingSite/js/script.js
+++ HikingSite/js/script.js
@@ -125,9 +125,9 @@ function createBlogList(items) {
       //html += '<p>' + content + '</p>';
       html += '</div>';
       html += '</div>';
-      console.log('--------------------------');
-      console.log(html);
-      console.log('--------------------------');
+      //console.log('--------------------------');
+      //console.log(html);
+      //console.log('--------------------------');
 
       $blogWrap.append(html);
     }
@@ -153,4 +153,42 @@ function createBlogList(items) {
       console.log(t);
     });*/
 
+/****************************************    Scroll plugin  ****************/
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+/*******************    End of scroll    *************************************/
 }
